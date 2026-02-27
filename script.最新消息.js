1cb765f129315e98b01d2fd1f60b7551df93d9b0
Nevebeauty0920 <v28455432@gmail.com>
2026-02-27 14:20:21 +0800

    Add files via upload

---
+++ script.最新消息.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector(".menu");
+    const links = document.querySelector(".links");
+    const overlay = document.querySelector(".overlay");
+    const closebtn = document.querySelector(".closebtn");
+    
+    // ====== 選單 ======
+    menu.addEventListener("click", () => {
+        links.classList.toggle("active");
+        overlay.classList.toggle("active");
+        document.querySelector(".news").classList.toggle("hide-text");
+    });
+
+    closebtn.addEventListener("click", () => {
+        links.classList.remove("active");
+        overlay.classList.remove("active");
+        document.querySelector(".news").classList.remove("hide-text");
+    });
+
+    overlay.addEventListener("click", () => {
+        links.classList.remove("active");
+        overlay.classList.remove("active");
+        document.querySelector(".news").classList.remove("hide-text");
+    });
+
+    document.querySelectorAll(".links a").forEach(link => {
+        link.addEventListener("click", () => {
+            links.classList.remove("active");
+            overlay.classList.remove("active");
+            document.querySelector(".news").classList.remove("hide-text");
+        });
+    });
+    document.querySelectorAll(".news-header").forEach(button => {
+    button.addEventListener("click", () => {
+        const item = button.parentElement;
+
+        // 如果你想一次只開一個，取消其他
+        document.querySelectorAll(".news-item").forEach(i => {
+            if (i !== item) i.classList.remove("active");
+        });
+
+        item.classList.toggle("active");
+    });
+   });
+});
